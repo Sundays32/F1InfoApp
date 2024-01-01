ca7c85094a541df8848d09013ffd9b93e21fa6d4
Sandesh <sandy.2211.r@gmail.com>
2024-01-01 21:39:32 +0530

    card

--- F1App/src/app/landing-page/landing-page.component.ts
+++ F1App/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
-
+  constructor(private renderer: Renderer2, private el: ElementRef){}
+  public scrollClick(){
+    // const target =this.el.nativeElement.querySelector("#underbanner");
+    // this.renderer.setProperty(target,'scrollTop',target.scrollTop+500)
+    window.scrollTo({top:710, behavior:'smooth'});
+    
+  }
+  
 }

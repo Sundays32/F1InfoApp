d29a0768ae6f82852c24e0d53b261c4a40278ade
Sandesh <sandy.2211.r@gmail.com>
2024-01-02 20:35:43 +0530

    Redbullpage

---
+++ F1App/src/app/teams/redbull/redbull.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redbull',
+  templateUrl: './redbull.component.html',
+  styleUrls: ['./redbull.component.scss']
+})
+export class RedbullComponent {
+
+}
